168733bdd0a4b5ceda03206d247577ab98aa3c35
windundsterne <jacob@windundsterne.de>
2022-01-23 11:52:43 +0100

    Serial Nutzung beseitigt damit UART0 frei ist

--- esp-kwb-mqttlogger.ino
+++ esp-kwb-mqttlogger.ino
@@ -180,26 +180,26 @@ void wifi_on()
       type = "filesystem";
     }
     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
+    //Serial.println("Start updating " + type);
   });
   ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
+    // Serial.println("\nEnd");
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
+      // Serial.println("Auth Failed");
     } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
+      // Serial.println("Begin Failed");
     } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
+      // Serial.println("Connect Failed");
     } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
+      // Serial.println("Receive Failed");
     } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
+      // Serial.println("End Failed");
     }
   });
 
@@ -471,11 +471,11 @@ void loop() {
 
   // Wichtig!!!
   // alle x Sekunden ## Update, damit OTA und Ping tun
-  if (milli > (timeru + 5 * 1000))
+  if (milli > (timeru + 15 * 1000))
   {
     idl[ic] += 100;
     timeru = milli;
-    ArduinoOTA.handle();
+    if (Kessel.ext == 0) ArduinoOTA.handle(); // OTA nur wenn Kessel nicht brennt
     client.loop(); // Zeit für den Callback+MQTT Ping
   }
 
@@ -749,4 +749,6 @@ void loop() {
 
   } // timer1 Ausgabe alle 5min
 
+// Delay am Loopende damit SW Watchdog nicht auslöst
+delay(5); 
 }
